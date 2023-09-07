8b1d66ead38ae712cc0574d29a39e828b196014d
LFAGC <53969231+LFAGC@users.noreply.github.com>
2023-09-08 01:52:35 +0200

    Some Navbar Changes

--- app/components/topnavbar.js
+++ app/components/topnavbar.js
@@ -16,7 +16,7 @@ let TopNavbar = function(Pagename) {
         <div className={`nav-container ${isNavOpen ? 'nav-container-open':''}`} >
             <div className={`current-page ${isNavOpen ? 'current-page-hide':''}`}>{Pagename['Pagename']}</div>
             <div className={`nav-selector-title ${isNavOpen ? 'nav-selector-title-show':''}`}>Navigatie</div>
-            <div className={`nav-selector-button`} onClick={setIsNavOpen}><i className='fa-solid fa-bars'></i></div>
+            <div className={`nav-selector-button ${isNavOpen ? 'nav-selector-button-hide':''}`} onClick={setIsNavOpen}><i className='fa-solid fa-bars'></i></div>
             <div className={`nav-selector-close  ${isNavOpen ? 'nav-selector-close-show':''}`} onClick={closeNav}><i className='fa-solid fa-xmark'></i></div>
             <a href='/' className={`nav-button ${isNavOpen ? 'nav-button-show':''}`}><p>Hoofdpagina</p></a>
             <a href='/about' className={`nav-button ${isNavOpen ? 'nav-button-show':''}`}><p>Over Mij</p></a>
