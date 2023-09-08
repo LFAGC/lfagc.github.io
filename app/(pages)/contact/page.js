cab49ca49473bb33df6603a2275025206d078042
LFAGC <53969231+LFAGC@users.noreply.github.com>
2023-09-08 18:40:37 +0200

    Fixed some server issues added projects component

---
+++ app/(pages)/contact/page.js
@@ -0,0 +1,19 @@
+import Input from 'postcss/lib/input.js';
+import Navbars from '../../components/navbar.js'
+
+import '../../styling/about.css';
+
+export let PageName = 'Contact'
+
+export const metadata = {
+    title: 'Lars Werner | Contact',
+    description: 'Hier kan je contact leggen met mij',
+}
+
+export default function Home() {
+    return (
+        <div className='w-3/5 absolute left-1/2 top-0 -translate-x-1/2 h-full contact-container'>
+            
+        </div>
+    )
+}
